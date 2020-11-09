a935da1e97df62245951c82262f72131c4655514
regisdossani <66984463+regisdossani@users.noreply.github.com>
2020-11-09 10:09:53 +0000

    Add files via upload

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,119 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Admin;
+use App\Equipe;
+use App\Formateur;
+use App\Apprenant;
+
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       // insert sample user as the system admin
+
+
+  $admin1=  Admin::create([
+        'username' => 'Régis',
+        'password' => bcrypt('passer123'),
+        'email' => 'admin1@email.com',]);
+
+$admin1->assignRole('superadmin');
+
+
+  $admin7=  Admin::create([
+        'username' => 'admin7',
+        'password' => bcrypt('passer'),
+        'email' => 'admin7@email.com',
+    ]);
+    $admin7->assignRole('superadmin');
+
+
+    $directeur=  Admin::create([
+        'username' => 'Directeur de Promess',
+        'password' => bcrypt('passer123'),
+        'email' => 'directeur@email.com',
+    ]);
+
+    $directeur->assignRole('superadmin');
+
+    $apprenant=  Apprenant::create([
+        'username' => 'apprenant1',
+        'password' => bcrypt('passer'),
+        'candidat_id'=> 1,
+        'email' => 'apprenant1@email.com',
+    ]);
+
+    $apprenant->assignRole('Apprenant');
+
+
+    $formateur=  Formateur::create([
+        'username' => 'formateur',
+        'password' => bcrypt('passer'),
+        'email' => 'formateur@email.com',
+    ]);
+
+    $formateur->assignRole('Formateur');
+
+
+    $membre1=  Equipe::create([
+        'username' => 'Responsable Pédagogique',
+        'password' => bcrypt('passer'),
+        'email' => 'membre1@email.com',
+    ]);
+
+    $membre1->assignRole('Resp-Pedagogique');
+
+    $membre2=  Equipe::create([
+        'username' => 'Responsable Administrative et Comptable',
+        'password' => bcrypt('passer'),
+        'email' => 'membre2@email.com',
+    ]);
+
+    $membre2->assignRole('Resp-Admin-Comptable');
+
+    $membre3=  Equipe::create([
+        'username' => 'Responsable Relations Extérieures',
+        'password' => bcrypt('passer'),
+        'email' => 'membre3@email.com',
+    ]);
+
+    $membre3->assignRole('Resp-Relation-Ext');
+
+
+    $membre4=  Equipe::create([
+        'username' => 'Responsable ULDLR',
+        'password' => bcrypt('passer'),
+        'email' => 'membre4@email.com',
+    ]);
+    $membre4->assignRole('Resp-ULDLR');
+
+
+    $membre5=  Equipe::create([
+        'username' => 'Responsable AccEd',
+        'password' => bcrypt('passer'),
+        'email' => 'membre5@email.com',
+    ]);
+
+    $membre5->assignRole('Responsable-AccEd');
+
+    $membre6=  Equipe::create([
+        'username' => 'Responsable Fond PVSYST',
+        'password' => bcrypt('passer'),
+        'email' => 'membre6@email.com',
+    ]);
+    $membre6->assignRole('Resp-Fond-PVSYST');
+
+
+
+
+    }
+}
---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,169 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+
+        $permissions = [
+            'role-lister',
+            'role-creer',
+            'role-modifier',
+            'role-supprimer',
+
+            'list-apprenant-postformation',
+            'apprenant-lister',
+           'apprenant-creer',
+           'apprenant-modifier',
+           'apprenant-supprimer',
+           'gestion-apprenants',
+
+           'gestion-planaffectation-formateur',
+           'gestion-referentiel',
+           'gestion-attendances-apprenant',
+           'gestion-test-entrée',
+           'gestion-stock-equipement-pedagogique',
+           'gestion-stock-equipement-postapprenant',
+           'gestion-notes-apprenant',
+
+           'voir-fiche-contact-persressource',
+           'voir-fichepartenariat',
+           'voir-suivi-echange',
+           'voir-apprenant-postformation',
+           'voir-animation-reseau',
+
+           'voir-echange-persressource',
+           'creer-echange-partenariat',
+           'modifier-echange-partenariat',
+           'supprimer-echange-partenariat',
+
+           'voir-candidat',
+            'candidat-lister',
+            'candidat-creer',
+            'candidat-modifier',
+            'candidat-supprimer',
+            'gestion-candidats',
+
+            'voir-formateur',
+            'formateur-lister',
+            'formateur-creer',
+            'formateur-modifier',
+            'formateur-supprimer',
+            'gestion-formateurs',
+
+            'professionnel-lister',
+            'professionnel-creer',
+            'professionnel-modifier',
+            'professionnel-supprimer',
+
+            'voir-stage',
+            'stage-lister',
+            'stage-creer',
+            'stage-modifier',
+            'stage-supprimer',
+            'gestion-stages',
+
+
+                'persressource-lister',
+            'persressource-creer',
+            'persressource-modifier',
+            'persressource-supprimer',
+
+          'equipe-lister',
+           'equipe-creer',
+           'equipe-modifier',
+           'equipe-supprimer',
+
+            'chantier-lister',
+            'chantier-creer',
+            'chantier-modifier',
+            'chantier-supprimer',
+            'voir-chantier',
+            'gestion-chantier',
+            'formation-lister',
+            'formation-creer',
+            'formation-modifier',
+            'formation-supprimer',
+
+            'typeformation-lister',
+            'typeformation-creer',
+            'typeformation-modifier',
+            'typeformation-supprimer',
+
+            'client-lister',
+            'client-creer',
+            'client-modifier',
+            'client-supprimer',
+
+            'parten-lister',
+            'parten-creer',
+            'parten-modifier',
+            'parten-supprimer',
+
+            'admin-lister',
+            'admin-creer',
+            'admin-modifier',
+            'admin-supprimer',
+
+            'type-lister',
+            'type-creer',
+            'type-modifier',
+            'type-supprimer',
+
+            'modifier-apprenant-postformation',
+            'supprimer-apprenant-postformation',
+            'creer-apprenant-postformation',
+            'gestion-apprenant-postformation',
+
+           'modifier-animation-reseau',
+           'supprimer-animation-reseau',
+           'creer-animation-reseau',
+           'gestion-animation-reseau',
+           'creer-fichepartenariat',
+           'modifier-fichepartenariat',
+           'supprimer-fichepartenariat',
+
+           'creer-fichechantier',
+           'modifier-fichechantier',
+           'supprimer-fichechantier',
+
+           'creer-fichestage',
+           'modifier-fichestage',
+           'supprimer-fichestage',
+           'gestion-stock',
+           'gestion-test-entree',
+
+
+           'creer-suivi-horaire-formateur',
+    'modifier-suivi-horaire-formateur',
+    'creer-suivi-horaire-equipe',
+    'modifier-suivi-horaire-equipe',
+
+
+    'modifier-contrat-prestataire',
+    'creer-contrat-prestataire',
+
+    'modifier-contrat-chantier',
+    'creer-contrat-chantier',
+
+    'modifier-formation-formateurs',
+    'modifier-stock-pedagogique',
+    'modifier-stock-apprenant-postformation'
+         ];
+
+
+         foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+    }
+}
+
+}
