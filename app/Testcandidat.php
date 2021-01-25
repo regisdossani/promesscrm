ccdffd710230a7a1b2d207d8c92960a6588f92a8
Regis <regisdossani@gmail.com>
2021-01-25 12:49:41 +0000

    candidat_id

--- app/Testcandidat.php
+++ app/Testcandidat.php
@@ -8,7 +8,7 @@ class Testcandidat extends Model
 {
     protected $fillable = [
         'filiere_id', 'test_ecrit', 'entretien','test_pj','resultat','signature',
-        'commentaire'
+        'commentaire','candidat_id'
     ];
     public function candidat(){
         return $this->belongsTo(Candidat::class,'candidat_id');
