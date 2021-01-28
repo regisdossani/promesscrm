7d69441a659b34208f8d395cf4817ea8e59b99e8
Regis <regisdossani@gmail.com>
2021-01-28 19:44:37 +0000

    Ok

--- resources/views/newchantiers/form.blade.php
+++ resources/views/newchantiers/form.blade.php
@@ -27,8 +27,8 @@
 <div class="col-md-6 mb-3 {{ $errors->has('etat') ? 'has-error' : ''}}">
     <label for="etat" class="control-label">{{ 'Etat' }}</label>
     <select class="form-control" name="etat"  id="etat" >
-        <option>-- --Choisissez un état--</option>
-            <option value="1">A</option>
+        <option value="">-- --Choisissez un état--</option>
+        <option value="1">A</option>
     </select>
 </div>
 <div class="col-md-6 mb-3 {{ $errors->has('valeur') ? 'has-error' : ''}}">
