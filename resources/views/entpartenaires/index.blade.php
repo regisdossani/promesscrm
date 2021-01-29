b57d80f28836bff9286932a6886728da3b3c04c3
Regis <regisdossani@gmail.com>
2021-01-29 21:07:53 +0000

    entpartenaires

--- resources/views/entpartenaires/index.blade.php
+++ resources/views/entpartenaires/index.blade.php
@@ -33,12 +33,12 @@
                         <div class="card-header">Liste des Entreprises</div>
                         <div class="panel-body">
 
-                            <a href="{{ url('/entreprises/create') }}" class="btn btn-success btn-sm" title="Add New entreprise">
+                            <a href="{{ url('/entpartenaires/create') }}" class="btn btn-success btn-sm" title="Add New entreprise">
                                 <i class="fa fa-plus" aria-hidden="true"></i> Nouveau
                             </a>
                             <div class="pull-right" style="margin-right:5px">
 
-                                <form method="GET" action="{{ url('/entreprises') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
+                                <form method="GET" action="{{ url('/entpartenaires') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
                                     <div class="form-group">
                                         <input type="text" class="form-control" name="search" placeholder="Rechercher..." value="{{ request('search') }}">
                                         <span class="form-group-append">
@@ -64,10 +64,10 @@
                                                 <td>{{ $loop->iteration }}</td>
                                                 <td>{{ $item->raison_sociale }}</td><td>{{ $item->reference }}</td><td>{{ $item->activite_entreprise }}</td>
                                                 <td>
-                                                    <a href="{{ url('/entreprises/' . $item->id) }}" title="Voir cette entreprise"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> </button></a>
-                                                    <a href="{{ url('/entreprises/' . $item->id . '/edit') }}" title="Modufier cette entreprise"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> </button></a>
+                                                    <a href="{{ url('/entpartenaires/' . $item->id) }}" title="Voir cette entreprise"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> </button></a>
+                                                    <a href="{{ url('/entpartenaires/' . $item->id . '/edit') }}" title="Modufier cette entreprise"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> </button></a>
 
-                                                    <form method="POST" action="{{ url('/entreprises' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    <form method="POST" action="{{ url('/entpartenaires' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                         {{ method_field('DELETE') }}
                                                         {{ csrf_field() }}
                                                         <button type="submit" class="btn btn-danger btn-sm" title="Supprimer cette entreprise" onclick="return confirm(&quot;Confirmez-vous la suppression??&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
