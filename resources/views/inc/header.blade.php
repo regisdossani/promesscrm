8f74d2e1fe943d145e477ff91042b50b5e69a40c
Regis <regisdossani@gmail.com>
2021-02-12 02:35:42 +0000

    Ok

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <!-- bootstrap-css -->
- {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" > --}}
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" >
 
         <!-- Custom CSS -->
         <link href="{{asset('css/style-responsive.css')}}" rel="stylesheet"/>
