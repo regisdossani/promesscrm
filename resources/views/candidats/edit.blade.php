7ab3f65eff90c24b13067df2fa9f1aef3ac86fe2
Regis <regisdossani@gmail.com>
2021-02-13 23:21:02 +0000

    OK

--- resources/views/candidats/edit.blade.php
+++ resources/views/candidats/edit.blade.php
@@ -13,20 +13,18 @@
 @section('content')
 <section id="main-content">
     <section class="wrapper">
-        <div class="form-w3layouts">
-            <div class="container">
-                    <div class="col-md-10">
-                        <section  class="panel">
+		<div class="table-agile-info">
+            <div class="panel panel-default">
                             <header class="panel-heading">
-                                <div class="panel-title">
                                     MODIFIER LE CANDIDAT
-                                </div>
                             </header>
 
-                        <div class="panel-body">
-                            <a href="{{ url('/candidats') }}" title="Précédent"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Précédent</button></a>
-                            <br />
-                            <br />
+                            <div class="row w3-res-tb">
+                                <div class="col-sm-5 m-b-xs">
+
+                                <a href="{{ url('/candidats') }}" title="Précédent"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Précédent</button></a>
+
+                                <br />
                                 <div class="row">
                                     @if ($errors->any())
                                         <ul class="alert alert-danger">
@@ -35,19 +33,17 @@
                                             @endforeach
                                         </ul>
                                     @endif
-                                    <div class="panel-body">
+                                </div>
+
                                         <form method="POST" action="{{ url('/candidats/' . $candidat->id) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
                                             {{ method_field('PATCH') }}
                                             {{ csrf_field() }}
 
                                             @include ('candidats.form', ['formMode' => 'edit'])
-
                                         </form>
-                                    </div>
-                                </div>
 
+                                    </div>
                         </div>
-                    </div>
             </div>
         </div>
     </section>
